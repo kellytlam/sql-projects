93ba7b5b0c45ddb9a0d16c3f2c3266ac48eaac53
kellytlam <kellytienlam@gmail.com>
2023-05-09 13:56:50 -0700

    Add queries for Tableau visualizations

--- CovidExploration.sql
+++ CovidExploration.sql
@@ -39,72 +39,33 @@ FROM CovidExploration..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER By 1, 2
 
--- Total Cases vs Total Population, Percentage of Population Infected by COVID, Vietnam and US
-SELECT 
-   location, 
-   date, 
-   total_cases, 
-   population, 
-   (total_cases/population)*100 AS infected_percentage
-FROM CovidExploration..CovidDeaths
-WHERE location = 'Vietnam' OR location = 'United States' AND continent IS NOT NULL
-ORDER By 1, 2
-
 -- Average percentage of population infected by COVID, Global
 SELECT 
    Location, 
-   AVG((total_cases/population))*100 AS infected_percentage
+   AVG((total_cases/population))*100 AS avg_infected_percentage
 FROM CovidExploration..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY 2 DESC
 
--- Average percentage of population infected by COVID in US and VN
-SELECT 
-   Location, 
-   AVG((total_cases/population))*100 AS infected_percentage
-FROM CovidExploration..CovidDeaths
-WHERE location = 'Vietnam' OR location = 'United States' AND continent IS NOT NULL
-GROUP BY location
-
--- Total Cases vs Total Deaths (shows likelihood of dying if you contract COVID in these countries)
+-- Total Cases vs Total Deaths (Fatality)
 SELECT 
    location, 
    date, 
    total_cases, 
    total_deaths, 
-   (total_deaths/total_cases)*100 AS death_percentage
+   (total_deaths/total_cases)*100 AS fatality_percentage
 FROM CovidExploration..CovidDeaths
 WHERE continent IS NOT NULL
-ORDER By 5 DESC
-
--- Total Cases vs Total Deaths (shows likelihood of dying if you contract COVID in VN, US)
-SELECT 
-   location, 
-   date, 
-   total_cases, 
-   total_deaths, 
-   (total_deaths/total_cases)*100 AS death_percentage
-FROM CovidExploration..CovidDeaths
-WHERE location = 'Vietnam' OR location = 'United States' AND continent IS NOT NULL
-ORDER By 5 DESC
 
 -- Average percentage of deaths by COVID, Global
 SELECT 
    location,
-   AVG((total_deaths/total_cases))*100 AS death_percentage
+   AVG((total_deaths/total_cases))*100 AS avg_death_percentage
 FROM CovidExploration..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY 2 DESC
-
--- Average percentage of deaths by COVID in US and VN
-SELECT 
-   location,
-   AVG((total_deaths/total_cases))*100 AS death_percentage
-FROM CovidExploration..CovidDeaths
-WHERE location = 'Vietnam' OR location = 'United States' AND continent IS NOT NULL
-GROUP BY location
 
 --Global Highest Infection Rate, Highest Percentage of Population Infected by COVID
 SELECT 
@@ -115,21 +76,7 @@ SELECT
 FROM CovidExploration..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY Location, Population
-ORDER By infection_percentage DESC
-
---Global Highest Infection Rate, Percentage of Population Infected by COVID
-SELECT 
-   Location, 
-   Population, 
-   MAX(total_cases) AS highest_infection_count, 
-   MAX((total_cases/population))*100 AS highest_infection_percentage
-FROM CovidExploration..CovidDeaths
-WHERE continent IS NOT NULL
-   AND location = 'Vietnam'
-   OR location = 'United States'
-GROUP BY Location, Population
-ORDER By infection_percentage DESC
-
+ORDER BY highest_infection_percentage DESC
 
 -- Countries with highest death count per population
 SELECT 
@@ -140,16 +87,6 @@ FROM CovidExploration..CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY Location
 ORDER By total_death_count DESC
-
--- Countries with highest death count per population, VN and US
-SELECT 
-   Location,
-   MAX(total_deaths) AS total_death_count
-FROM CovidExploration..CovidDeaths
-WHERE Location = 'Vietnam' OR Location = 'United States' AND continent IS NOT NULL
-GROUP BY Location
-ORDER By total_death_count DESC
-
 
 /* Vaccinations */
 
@@ -195,8 +132,8 @@ AS
       ON dea.location = vac.location
       AND dea.date = vac.date
    WHERE dea.continent IS NOT NULL 
-      AND dea.location = 'Vietnam'
-      OR dea.location = 'United States'
+      --AND dea.location = 'Vietnam'
+      --OR dea.location = 'United States'
    )
 SELECT *, (rolling_new_vaccinations/population)*100 AS new_vaccinated_percentage
 FROM PopvsVac
@@ -204,5 +141,64 @@ ORDER BY 7 DESC
 
 
 
+/* Queries for Tableau Visualization */
+
+-- Highest Infection and Percent of Population Infected by Country
+
+SELECT
+   location,
+   population,
+   MAX(total_cases) AS highest_infection_count,
+   MAX((total_cases/population))*100 AS pop_percent_infected
+FROM CovidExploration..CovidDeaths
+GROUP BY location, population
+ORDER BY pop_percent_infected DESC
+
+-- Highest Infection and Percent of Population Infected by Country with date
+
+SELECT
+   location,
+   population,
+   date,
+   MAX(total_cases) AS highest_infection_count,
+   MAX((total_cases/population))*100 AS pop_percent_infected
+FROM CovidExploration..CovidDeaths
+GROUP BY location, population, date
+ORDER BY pop_percent_infected DESC
+
+-- Fully Vaccinated Percentage
+SELECT
+   location,
+   population,
+   MAX(people_fully_vaccinated) AS vaccination_count,
+   MAX((people_fully_vaccinated/population))*100 AS vaccination_percentage
+FROM CovidExploration..CovidVaccinations
+GROUP BY location, population
+ORDER BY 4 DESC
+
+-- Total Average Percentage of Population Infected, Mortality, Fatality, and Vaccination 
+SELECT
+   AVG((dea.total_cases/dea.population))*100 AS avg_infected_percentage,
+   AVG((dea.total_deaths/dea.population))*100 AS avg_mortality_percentage,
+   AVG((dea.total_deaths/dea.total_cases))*100 AS avg_fatality_percentage,
+   AVG((vac.people_fully_vaccinated/dea.population))*100 AS avg_vaccination_percentage
+FROM CovidExploration..CovidDeaths dea
+JOIN CovidExploration..CovidVaccinations vac
+   ON dea.location = vac.location
+   AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
 
 
+-- Total Average Percentage of Population Infected, Mortality, Fatality, and Vaccination (SEA Countries)
+SELECT
+   AVG((dea.total_cases/dea.population))*100 AS avg_infected_percentage,
+   AVG((dea.total_deaths/dea.population))*100 AS avg_mortality_percentage,
+   AVG((dea.total_deaths/dea.total_cases))*100 AS avg_fatality_percentage,
+   AVG((vac.people_fully_vaccinated/dea.population))*100 AS avg_vaccination_percentage
+FROM CovidExploration..CovidDeaths dea
+JOIN CovidExploration..CovidVaccinations vac
+   ON dea.location = vac.location
+   AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+   AND dea.location IN ('Brunei', 'Myanmar', 'Indonesia', 'Vietnam', 'Cambodia', 'Laos', 
+                        'Timor', 'Thailand', 'Singapore', 'Malaysia', 'Philippines')
